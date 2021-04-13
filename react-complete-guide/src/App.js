8cfbe894daedd5a58dff66ee1638e625a0d52cf6
Patrycja Kierejewska <patikier@o2.pl>
2021-04-13 21:58:24 +0200

    styling components and elements with radium and styled-components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+// import Radium from "radium";
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  
+  &:hover {
+      background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+      color: black;
+`;
 
 class App extends Component {
     state = {
@@ -45,13 +60,18 @@ class App extends Component {
     }
 
     render () {
-        const style = {
-          backgroundColor: 'white',
-          font: 'inherit',
-          border: '1px solid blue',
-          padding: '8px',
-          cursor: 'pointer'
-        };
+        // const style = {
+        //   backgroundColor: 'green',
+        //   color: 'white',
+        //   font: 'inherit',
+        //   border: '1px solid blue',
+        //   padding: '8px',
+        //   cursor: 'pointer',
+        //   ':hover': {
+        //       backgroundColor: 'lightgreen',
+        //       color: 'black'
+        //   }
+        // };
 
         let persons = null;
 
@@ -69,15 +89,32 @@ class App extends Component {
                     })}
                 </div>
             );
+
+            // style.backgroundColor = 'red';
+            // style[':hover'] = {
+            //     backgroundColor: 'salmon',
+            //         color: 'black'
+            // };
+        }
+
+        const classes = []; // result -> "red bold"
+        if (this.state.persons.length <= 2) {
+            classes.push('red'); //classes = ['red']
+        }
+        if(this.state.persons.length <= 1) {
+            classes.push('bold'); // classes = ['red', 'bold']
         }
 
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
-                <button
-                    style={style}
-                    onClick={this.togglePersonsHandler}>Toggle persons</button>
+                <p className={classes.join(' ')}>This is really working!</p>
+                {/*<button*/}
+                {/*    style={style}*/}
+                {/*    onClick={this.togglePersonsHandler}>Toggle persons</button>*/}
+                <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler} >
+                    Toggle Persons
+                </StyledButton>
                 {persons}
             </div>
         );
@@ -85,4 +122,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default App; //higher component
