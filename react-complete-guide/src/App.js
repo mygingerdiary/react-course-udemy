d1d7749703763d61c7aba44e6ade60f6737450be
Patrycja Kierejewska <patikier@o2.pl>
2021-04-21 14:54:07 +0200

    rendering lists & conditional content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,37 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+    {
+        id: "e1",
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28)
+    },
+    {
+        id: "e2",
+        title: 'Toilet paper',
+        amount: 3.20,
+        date: new Date(2020, 3, 12)
+    },
+    {
+        id: "e3",
+        title: 'Fruit',
+        amount: 10.03,
+        date: new Date(2021, 1, 3)
+    },
+    {
+        id: "e4",
+        title: 'Something',
+        amount: 123.44,
+        date: new Date(2021, 2, 28)
+    }
+];
+
 const App = () => {
-    const expenses = [
-        {
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28)
-        },
-        {
-            title: 'Toilet paper',
-            amount: 3.20,
-            date: new Date(2020, 3, 12)
-        },
-        {
-            title: 'Fruit',
-            amount: 10.03,
-            date: new Date(2021, 1, 3)
-        },
-        {
-            title: 'Something',
-            amount: 123.44,
-            date: new Date(2021, 2, 28)
-        }
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpenseHandler = expense => {
-        console.log('In App.js');
-        console.log(expense);
-    }
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses]
+        });
+    };
 
     return (
         <div>
--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEdititing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -11,11 +13,21 @@ const NewExpense = (props) => {
         };
 
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
 
     return(
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEdititing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEdititing && <ExpenseForm onCancel={stopEditingHandler} onSaveExpenseData={saveExpenseDataHandler} />}
         </div>
     );
 }
