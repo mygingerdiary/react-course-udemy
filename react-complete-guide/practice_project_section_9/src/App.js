94e7cdd2406433f7b215cd1619ac1ac7ea017232
Patrycja Kierejewska <patikier@o2.pl>
2021-04-23 18:49:25 +0200

    project to repeat previous material

---
+++ react-complete-guide/practice_project_section_9/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+      setUsersList(prevState => {
+          return [...prevState, {id: Math.random().toString(), name: uName, age: uAge}];
+      });
+  }
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList}/>
+    </div>
+  );
+}
+
+export default App;
