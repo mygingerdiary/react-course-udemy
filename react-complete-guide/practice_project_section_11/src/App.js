262623e9a10e272dc394ec436b7c42635d18ad62
Patrycja Kierejewska <patikier@o2.pl>
2021-05-02 22:55:50 +0200

    styling and creating components

--- react-complete-guide/practice_project_section_11/src/App.js
+++ react-complete-guide/practice_project_section_11/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <Cart/>
+            <Header/>
+            <main>
+                <Meals/>
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
