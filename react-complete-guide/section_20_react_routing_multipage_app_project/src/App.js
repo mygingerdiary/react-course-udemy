72c5f8ad0d6ce5d691b236907c90f1d9d39a234f
Patrycja Kierejewska <patikier@o2.pl>
2021-06-09 16:07:34 +0200

    adding easy routing

--- react-complete-guide/section_20_react_routing_multipage_app_project/src/App.js
+++ react-complete-guide/section_20_react_routing_multipage_app_project/src/App.js
@@ -1,9 +1,19 @@
+import {Route} from 'react-router-dom';
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+        <Route path="/welcome">
+            <Welcome />
+        </Route>
+        <Route path="/products">
+            <Products />
+        </Route>
     </div>
   );
 }
 
 export default App;
+
