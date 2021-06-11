a13b689b785cf6fec6e04fc957531d23d239d8ab
Patrycja Kierejewska <patikier@o2.pl>
2021-06-11 17:34:03 +0200

    working with query parameters to sort quotes in a proper way, working with nested routes

--- react-complete-guide/section_20_react_routing/practice project - SPA/src/pages/QuoteDetail.js
+++ react-complete-guide/section_20_react_routing/practice project - SPA/src/pages/QuoteDetail.js
@@ -1,9 +1,8 @@
-import {Route, useParams} from 'react-router-dom';
+import {Link, Route, useParams} from 'react-router-dom';
 
 import {Fragment} from "react";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 const DUMMY_QUOTES = [
     { id: 'q1', author: 'Patrycja', text: 'Reast is fun!'},
@@ -22,6 +21,13 @@ const QuoteDetail = () => {
     return(
         <Fragment>
             <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+                        Load Comments
+                    </Link>
+                </div>
+            </Route>
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
