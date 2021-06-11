0c90823186415b86de52a2ddd72b8f0bf7e5613e
Patrycja Kierejewska <patikier@o2.pl>
2021-06-11 14:28:08 +0200

    creating main routes

--- react-complete-guide/section_20_react_routing/practice project - SPA/src/App.js
+++ react-complete-guide/section_20_react_routing/practice project - SPA/src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div>
+import {Route, Switch} from 'react-router-dom';
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 
-    </div>
-  );
+function App() {
+    return (
+        <Switch>
+            <Route path='/quotes' exact>
+                <AllQuotes />
+            </Route>
+            <Route path='/quotes/:quoteId'>
+                <QuoteDetail />
+            </Route>
+            <Route path='/new-quote'>
+                <NewQuote />
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
