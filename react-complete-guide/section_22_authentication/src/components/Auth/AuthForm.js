e860bfc77cbf9a2988f2c5ae0cfa37a1a56314aa
Patrycja Kierejewska <patikier@o2.pl>
2021-06-17 12:55:11 +0200

    add login functionality and managing navbar due to results of login

--- react-complete-guide/section_22_authentication/src/components/Auth/AuthForm.js
+++ react-complete-guide/section_22_authentication/src/components/Auth/AuthForm.js
@@ -1,12 +1,16 @@
-import {useState, useRef} from 'react';
+import {useState, useRef, useContext} from 'react';
 
 import classes from './AuthForm.module.css';
+import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
 
+    const authCtx = useContext(AuthContext);
+
     const [isLogin, setIsLogin] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
 
     const switchAuthModeHandler = () => {
         setIsLogin((prevState) => !prevState);
@@ -18,33 +22,48 @@ const AuthForm = () => {
         const enteredEmail = emailInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
 
+        setIsLoading(true);
+        let url;
         if (isLogin) {
-
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDjpq-P0O7BTC5LCTGrTJyVNxXxK11z6TU';
         } else {
-            fetch(
-                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDjpq-P0O7BTC5LCTGrTJyVNxXxK11z6TU',
-                {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        email: enteredEmail,
-                        password: enteredPassword,
-                        returnSecureToken: true,
-                    }),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-            ).then(res => {
-                if (res.ok) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDjpq-P0O7BTC5LCTGrTJyVNxXxK11z6TU';
+        }
 
+        fetch(
+            url,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: enteredEmail,
+                    password: enteredPassword,
+                    returnSecureToken: true,
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => {
+                setIsLoading(false);
+                if (res.ok) {
+                    return res.json();
                 } else {
                     return res.json().then(data => {
-                        //show error
-                        console.log(data);
+                        const errorMessage = 'Authentication failes!';
+                        // if (data && data.error && data.error.message) {
+                        //     errorMessage = data.error.message;
+                        // }
+                        throw new Error(errorMessage);
                     });
                 }
+            })
+            .then(data => {
+                authCtx.login(data.idToken);
+            })
+            .catch(err => {
+                alert(err.message);
+
             });
-        }
     };
 
     return (
@@ -60,7 +79,8 @@ const AuthForm = () => {
                     <input type='password' id='password' ref={passwordInputRef} required/>
                 </div>
                 <div className={classes.actions}>
-                    <button>{isLogin ? 'Login' : 'Create Account'}</button>
+                    {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+                    {isLoading && <p>Loading...</p>}
                     <button
                         type='button'
                         className={classes.toggle}
