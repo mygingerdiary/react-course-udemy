8d39229f88dffca477b40591bfa0d66660a639cb
Patrycja Kierejewska <patikier@o2.pl>
2021-06-27 17:42:59 +0200

    custom hooks

--- react-complete-guide/section_26_replacing_redux_with_react_hooks/src/components/Products/ProductItem.js
+++ react-complete-guide/section_26_replacing_redux_with_react_hooks/src/components/Products/ProductItem.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import Card from '../UI/Card';
+import {useStore} from "../hooks-store/store";
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
 
 const ProductItem = props => {
-  const dispatch = useDispatch();
+    const dispatch = useStore(false)[1];
 
-  const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
-  };
+    const toggleFavHandler = () => {
+        dispatch('TOGGLE_FAV', props.id);
+    };
 
-  return (
-    <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
-        <p>{props.description}</p>
-        <button
-          className={!props.isFav ? 'button-outline' : ''}
-          onClick={toggleFavHandler}
-        >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
-        </button>
-      </div>
-    </Card>
-  );
+    return (
+        <Card style={{marginBottom: '1rem'}}>
+            <div className="product-item">
+                <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+                <p>{props.description}</p>
+                <button
+                    className={!props.isFav ? 'button-outline' : ''}
+                    onClick={toggleFavHandler}
+                >
+                    {props.isFav ? 'Un-Favorite' : 'Favorite'}
+                </button>
+            </div>
+        </Card>
+    );
 };
 
 export default ProductItem;
--- react-complete-guide/section_26_replacing_redux_with_react_hooks/src/containers/Favorites.js
+++ react-complete-guide/section_26_replacing_redux_with_react_hooks/src/containers/Favorites.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import FavoriteItem from '../components/Favorites/FavoriteItem';
+import {useStore} from "../components/hooks-store/store";
 import './Products.css';
 
 const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
+  const state = useStore()[0];
+  const favoriteProducts = state.products.filter(p => p.isFavorite);
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
-      <ul className="products-list">
-        {favoriteProducts.map(prod => (
-          <FavoriteItem
-            key={prod.id}
-            id={prod.id}
-            title={prod.title}
-            description={prod.description}
-          />
-        ))}
-      </ul>
+        <ul className="products-list">
+          {favoriteProducts.map(prod => (
+              <FavoriteItem
+                  key={prod.id}
+                  id={prod.id}
+                  title={prod.title}
+                  description={prod.description}
+              />
+          ))}
+        </ul>
     );
   }
   return content;
