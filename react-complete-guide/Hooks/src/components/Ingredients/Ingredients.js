07c805533aa2f6f0130eedb705e0aabadff22d7e
Patrycja Kierejewska <patikier@o2.pl>
2021-06-28 21:29:24 +0200

    sending http requests
    working with firebase

--- react-complete-guide/Hooks/src/components/Ingredients/Ingredients.js
+++ react-complete-guide/Hooks/src/components/Ingredients/Ingredients.js
@@ -8,10 +8,20 @@ function Ingredients() {
     const [userIngredients, setUserIngredients] = useState([]);
 
     const addIngredientHandler = (ingredient) => {
-        setUserIngredients(prevIngredients => [
-            ...prevIngredients,
-            {id: Math.random().toString(), ...ingredient}
-        ])
+        fetch('https://hooks-react-2915b-default-rtdb.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify({ingredient}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            return response.json();
+        }).then(responseData => {
+            setUserIngredients(prevIngredients => [
+                ...prevIngredients,
+                {id: responseData.name, ...ingredient}
+            ])
+        });
     };
 
     const removeIngredientHandler = (ingredientId) => {
