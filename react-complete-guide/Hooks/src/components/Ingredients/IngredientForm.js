c1b26f9e844bebd82c11e990918ffa08624589c6
Patrycja Kierejewska <patikier@o2.pl>
2021-06-28 21:19:54 +0200

    creating functions using useState() hook

--- react-complete-guide/Hooks/src/components/Ingredients/IngredientForm.js
+++ react-complete-guide/Hooks/src/components/Ingredients/IngredientForm.js
@@ -9,7 +9,8 @@ const IngredientForm = React.memo(props => {
 
     const submitHandler = event => {
         event.preventDefault();
-        // ...
+
+        props.onAddIngredient({title: enteredTitle, amount: enteredAmount});
     };
 
     return (
--- react-complete-guide/Hooks/src/components/Ingredients/Ingredients.js
+++ react-complete-guide/Hooks/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from "./IngredientList";
 import Search from './Search';
 
 function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+    const [userIngredients, setUserIngredients] = useState([]);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
+    const addIngredientHandler = (ingredient) => {
+        setUserIngredients(prevIngredients => [
+            ...prevIngredients,
+            {id: Math.random().toString(), ...ingredient}
+        ])
+    };
+
+    const removeIngredientHandler = (ingredientId) => {
+        const updatedIngredients = userIngredients.filter(ig => ig.id !== ingredientId);
+
+        setUserIngredients(updatedIngredients);
+    }
+
+
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredientHandler}/>
+
+            <section>
+                <Search/>
+                <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+            </section>
+        </div>
+    );
 }
 
 export default Ingredients;
