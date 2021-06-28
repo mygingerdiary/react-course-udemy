3a01e23b43d91d19b92b0a3a56ba99a21525bf52
Patrycja Kierejewska <patikier@o2.pl>
2021-06-28 11:46:52 +0200

    removing items

--- react-complete-guide/section_27_typescript/react-ts/src/components/Todos.tsx
+++ react-complete-guide/section_27_typescript/react-ts/src/components/Todos.tsx
@@ -4,11 +4,15 @@ import TodoItem from "./TodoItem";
 
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{items: Todo[]}> = (props) => {
+const Todos: React.FC<{items: Todo[]; onRemoveTodo: (id: string) => void}> = (props) => {
     return (
         <ul className={classes.todos}>
             {props.items.map(item => (
-                <TodoItem key={item.id} todoText={item.text}/>
+                <TodoItem
+                    key={item.id}
+                    todoText={item.text}
+                    onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+                />
             ))}
         </ul>
     );
